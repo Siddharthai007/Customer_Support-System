72b02e37c1f6e4b59bbc66daa293312fa184f903
Siddharthai007 <siddharthasankarroy27@gmail.com>
2025-05-12 00:01:19 +0530

    code updated for data reading using panda

--- data_ingestion/data_transform.py
+++ data_ingestion/data_transform.py
@@ -5,7 +5,16 @@ from langchain_core.documents import Document
 
 class data_converter:
     def __init__(self):
-        pass
+        print("data converter has been initiated")
+        self.product_data=pd.read_csv(r"/Users/siddhartha/Desktop/SQL007/RAG/Customer_Support System/data/flipkart_product_review.csv")
+        print(self.product_data.head())
+
+
     def data_transformation(self):
         pass
+
+if __name__== '__main__' :
+    data_con=data_converter()
+
+
     
