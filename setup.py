107a952cc92e6a460dda48475716f2f0caf50f53
Siddharthai007 <siddharthasankarroy27@gmail.com>
2025-05-11 23:00:55 +0530

    setup.py file updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(name="e-commerce-bot",
+      version="0.0.1",
+      author="siddhartha",
+      author_email="siddharthasankarroy27@gmail.com",
+      packages=find_packages(),
+      install_requires=[
+          'langchain-astradb','langchain'])
